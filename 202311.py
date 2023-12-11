6aa1d335dcf7cfd0a95adf9ad921a90f5fa48a1d
biesnecker <jbiesnecker@gmail.com>
2023-12-11 09:14:49 -0500

    202311

---
+++ 202311.py
@@ -0,0 +1,42 @@
+import itertools
+
+grid = {}
+with open("202311.txt", "r") as f:
+    for y, line in enumerate(f):
+        for x, char in enumerate(line.strip()):
+            if char == "#":
+                grid[(x, y)] = char
+
+
+def find_distances(grid, expansion_factor):
+    min_x = min(x for (x, _) in grid.keys())
+    max_x = max(x for (x, _) in grid.keys())
+    min_y = min(y for (_, y) in grid.keys())
+    max_y = max(y for (_, y) in grid.keys())
+
+    empty_cols = set()
+    for x in range(min_x, max_x + 1):
+        if all((x, y) not in grid for y in range(min_y, max_y + 1)):
+            empty_cols.add(x)
+    empty_rows = set()
+    for y in range(min_y, max_y + 1):
+        if all((x, y) not in grid for x in range(min_x, max_x + 1)):
+            empty_rows.add(y)
+
+    total = 0
+    for (ix, iy), (jx, jy) in itertools.combinations(grid.keys(), 2):
+        [min_x, max_x] = sorted([ix, jx])
+        [min_y, max_y] = sorted([iy, jy])
+        total += (max_x - min_x) + sum(
+            expansion_factor for x in range(min_x, max_x + 1) if x in empty_cols
+        )
+        total += (max_y - min_y) + sum(
+            expansion_factor for y in range(min_y, max_y + 1) if y in empty_rows
+        )
+    return total
+
+
+print(f"Part one: {find_distances(grid, 1)}")
+
+# Love me some off-by-one errors.
+print(f"Part two: {find_distances(grid, 1000000-1)}")
