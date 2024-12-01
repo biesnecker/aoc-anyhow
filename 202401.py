7ec4c280bebbbe8fa9c00e499121b64c17760aba
John Biesnecker <jbiesnecker@meta.com>
2024-12-01 00:34:45 -0500

    comprehensions

--- 202401.py
+++ 202401.py
@@ -5,24 +5,18 @@ right = []
 
 with open("202401.txt", "r") as f:
     for line in f:
-        [a, b] = line.split()
-        left.append(int(a))
-        right.append(int(b))
+        [a, b] = list(map(int, line.split()))
+        left.append(a)
+        right.append(b)
 
     left.sort()
     right.sort()
 
-part_one = 0
-
-for i in range(len(left)):
-    part_one += abs(right[i] - left[i])
+part_one = sum(abs(x - y) for (x, y) in zip(left, right))
 
 print(f"Part one: {part_one}")
 
-part_two = 0
 c = Counter(right)
-
-for v in left:
-    part_two += c[v] * v
+part_two = sum(v * c[v] for v in left)
 
 print(f"Part two: {part_two}")
