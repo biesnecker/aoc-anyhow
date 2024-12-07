4daba28f8166d54f9d7e45ca0261af9d7a747f4e
John Biesnecker <jbiesnecker@meta.com>
2024-12-07 22:26:46 +0100

    Do it backwards

--- 202407.py
+++ 202407.py
@@ -1,3 +1,4 @@
+import math
 from collections import defaultdict
 from typing import DefaultDict, List, Tuple
 
@@ -12,40 +13,54 @@ for line in input_as_strings_iter("202407.txt"):
         inp[int(total)].append(part)
 
 
-def find_solution(
-    total: int, current: int, parts: List[int], pidx: int, part_two: bool
-) -> bool:
-    if pidx == len(parts):
-        return total == current
-    if current > total:
-        return False
+def find_solution_backwards(total: int, parts: List[int]) -> Tuple[bool, bool]:
+    s: List[Tuple[int, int, bool]] = [(total, len(parts) - 1, False)]
 
-    if pidx == 0:
-        return find_solution(total, current + parts[pidx], parts, pidx + 1, part_two)
+    found_one = False
+    found_two = False
 
-    # Try the sum
-    if find_solution(total, current + parts[pidx], parts, pidx + 1, part_two):
-        return True
+    while s:
+        current, pidx, used_two = s.pop()
 
-    # Try the product
-    if find_solution(total, current * parts[pidx], parts, pidx + 1, part_two):
-        return True
+        assert pidx >= 0
 
-    # Try the concat
-    if part_two and find_solution(
-        total, int(str(current) + str(parts[pidx])), parts, pidx + 1, part_two
-    ):
-        return True
-    return False
+        if pidx == 0:
+            if current == parts[pidx]:
+                if used_two:
+                    found_two = True
+                else:
+                    # Part one solutions work for both
+                    found_one = True
+                    found_two = True
+                if found_one and found_two:
+                    # short-circuit
+                    return (True, True)
+            continue
+
+        if current % parts[pidx] == 0:
+            # We can divide
+            s.append((current // parts[pidx], pidx - 1, used_two))
+        if current - parts[pidx] >= 0:
+            # We can subtract
+            s.append((current - parts[pidx], pidx - 1, used_two))
+
+        c = str(current)
+        p = str(parts[pidx])
+        if c.endswith(p) and len(c) > len(p):
+            # We can de-concat
+            s.append((int(c.removesuffix(p)), pidx - 1, True))
+
+    return (found_one, found_two)
 
 
 part_one = 0
 part_two = 0
 for total, parts in inp.items():
-    if find_solution(total, 0, parts, 0, False):
+    one, two = find_solution_backwards(total, parts)
+    if one:
         part_one += total
         part_two += total
-    elif find_solution(total, 0, parts, 0, True):
+    elif two:
         part_two += total
 print(f"Part one: {part_one}")
 print(f"Part one: {part_two}")
