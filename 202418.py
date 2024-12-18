8806e1516fe511911b9d00a7f9b5b91e93d324d8
Your Name <jbiesnecker@gmail.com>
2024-12-18 08:47:23 -0500

    202418

---
+++ 202418.py
@@ -0,0 +1,50 @@
+from utils import (
+    input_as_list_of_numbers_iter,
+    xy_to_coord,
+    coord_to_xy,
+    get_neighbors_cardinal,
+)
+import heapq
+
+input = []
+for [x, y] in input_as_list_of_numbers_iter("202418.txt", split_on=","):
+    input.append(xy_to_coord(x, y))
+
+
+def can_navigate(steps):
+    grid = {c for c in input[:steps]}
+    q = [(0, 0, 0)]
+    cheapest = {xy_to_coord(0, 0): 0}
+    target = xy_to_coord(70, 70)
+    x_bound = 70
+    y_bound = 70
+    while q:
+        cost, x, y = heapq.heappop(q)
+        pos = xy_to_coord(x, y)
+        if pos == target:
+            return cost
+        for _, npos in get_neighbors_cardinal(pos):
+            x, y = coord_to_xy(npos)
+            if x < 0 or y < 0 or x > x_bound or y > y_bound or npos in grid:
+                continue
+            if npos in cheapest and cheapest[npos] <= cost + 1:
+                continue
+            cheapest[npos] = cost + 1
+            heapq.heappush(q, (cost + 1, x, y))
+    return None
+
+
+part_one = can_navigate(1024)
+assert part_one is not None
+print("Part One:", part_one)
+
+i = 1024
+j = len(input) - 1
+while i < j:
+    mid = (i + j) // 2
+    if can_navigate(mid) is None:
+        j = mid - 1
+    else:
+        i = mid
+x, y = coord_to_xy(input[i])
+print(f"Part Two: {x},{y}")
