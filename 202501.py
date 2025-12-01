2394c7b3ac70ace5758bd5b8ddd6c352a4bcebd3
John Biesnecker <john.biesnecker@airbnb.com>
2025-12-01 09:33:37 -0500

    202501 refactor

--- 202501.py
+++ 202501.py
@@ -9,18 +9,28 @@ with open("202501.txt", "r") as f:
         amt = int(line[1:])
         input.append((d, amt))
 
+# Takes the input and the start position and returns the end position
+# and the number of full rotations
+def rotate(row: Tuple[str, int], pos: int) -> Tuple[int, int]:
+    (d, amt) = row
+    rots = amt // 100
+    rest = amt % 100
+    if d == "L":
+        if rest >= pos and pos != 0:
+            rots += 1
+        pos = (pos - rest) % 100
+    else:
+        if rest >= (100 - pos) and pos != 0:
+            rots += 1
+        pos = (pos + rest) % 100
+    return (pos, rots)
+
 
 def partOne(input: list[Tuple[str, int]]) -> int:
     res = 0
     pos = 50
     for row in input:
-        match row:
-            case ("L", amt):
-                pos = (pos - amt) % 100
-            case ("R", amt):
-                pos = (pos + amt) % 100
-            case _:
-                raise Exception(f"Invalid input: {row}")
+        pos = rotate(row, pos)[0]
         if pos == 0:
             res += 1
     return res
@@ -30,23 +40,8 @@ def partTwo(input: list[Tuple[str, int]]) -> int:
     res = 0
     pos = 50
     for row in input:
-        match row:
-            case ("L", amt):
-                rots = amt // 100
-                rest = amt % 100
-                res += rots
-                if rest >= pos and pos != 0:
-                    res += 1
-                pos = (pos - rest) % 100
-            case ("R", amt):
-                rots = amt // 100
-                rest = amt % 100
-                res += rots
-                if rest >= (100 - pos) and pos != 0:
-                    res += 1
-                pos = (pos + rest) % 100
-            case _:
-                raise Exception(f"Invalid input: {row}")
+        pos, rots = rotate(row, pos)
+        res += rots
     return res
 
 
